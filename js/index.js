859a5dd3b46ec56798a5fa022b81043963707b85
Stavros Gogos <sgogos1@gmail.com>
2021-07-01 21:29:10 -0700

    [MODIFIED] Arc to be responsive to either portrait or landscape and for the text to disappear at the bottom of the page.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 const body = document.body;
+const arcSvg = document.getElementById("arc-svg");
 
 function pageSize(){    
     if (window.innerWidth <= window.innerHeight){
         body.classList.add("portrait");
         body.classList.remove("landscape");
+        arcSvg.setAttribute("viewBox", "10 -40 220 320");
+        arcSvg.setAttribute("preserveAspectRatio", "none");
     }
     else {
         body.classList.remove("portrait");
         body.classList.add("landscape");
+        arcSvg.setAttribute("viewBox", "0 -15 235.46 300");
+        arcSvg.removeAttribute("preserveAspectRatio");
     }
 }
 
