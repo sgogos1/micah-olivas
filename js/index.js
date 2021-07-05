4fae288b47de7e3bd5773815eaa0ff9841704423
Stavros Gogos <sgogos1@gmail.com>
2021-07-05 12:44:31 -0700

    [MODIFIED] Responsiveness of arc, text, logo, and calendar. Made them all respond to vh instead of vmax and have static values so that they respond with vertical page size. Removed portrait and landscape classes as well.

--- js/index.js
+++ js/index.js
@@ -2,17 +2,17 @@ const body = document.body;
 const arcSvg = document.getElementById("arc-svg");
 
 function pageSize(){    
-    if (window.innerWidth <= window.innerHeight){
-        body.classList.add("portrait");
-        body.classList.remove("landscape");
+    if (((window.innerWidth - window.innerHeight) > -200) && ((window.innerWidth - window.innerHeight) < 200)){
+        arcSvg.setAttribute("viewBox", "-55 -40 350 310");   
+        arcSvg.setAttribute("preserveAspectRatio", "none");
+    }
+    else if (window.innerWidth <= window.innerHeight){
         arcSvg.setAttribute("viewBox", "10 -40 220 320");
         arcSvg.setAttribute("preserveAspectRatio", "none");
     }
     else {
-        body.classList.remove("portrait");
-        body.classList.add("landscape");
-        arcSvg.setAttribute("viewBox", "0 -15 235.46 300");
-        arcSvg.removeAttribute("preserveAspectRatio");
+        arcSvg.setAttribute("viewBox", "0 -20 235.46 300");
+        arcSvg.removeAttribute("preserveAspectRatio", "none");
     }
 }
 window.onresize = pageSize;
